c7489c70e5af2fe817876085f44e30fdbaa09816
OTR Study <stas.basov@whatever.com>
2022-09-22 02:02:14 -0700

    cosmetics changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,7 @@
 const router = require('express').Router();
 const { celebrate } = require('celebrate');
-const {
-  createUser,
-  login,
-} = require('../controllers/users');
-const {
-  ruleCreateUser,
-  ruleLogin,
-} = require('../validators/users');
+const { createUser, login } = require('../controllers/users');
+const { ruleCreateUser, ruleLogin } = require('../validators/users');
 
 router.post('/signin', celebrate(ruleLogin), login);
 router.post('/signup', celebrate(ruleCreateUser), createUser);
